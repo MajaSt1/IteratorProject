dbee6437191a9ddded546c41b6c1d31a05efac81
Maja Stasinska <maja.stasinska@capgemini.com>
2019-04-04 14:44:23 +0200

    zadanie1

---
+++ zadanie1/src/datastructure/list/Node.java
@@ -0,0 +1,8 @@
+package datastructure.list;
+
+/**
+ * Component of linked list that stores a value and reference to the next element.
+ */
+public class Node {
+      /* (TODO Starterkit 1) Please introduce a sensible implementation */
+}
